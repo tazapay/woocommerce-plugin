586ccfeaea827e3fa80c1da9b1b5cdd150918e33
Suraj <suraj@tazapay.com>
2024-10-29 12:54:47 +0530

    fix block icons

--- includes/block-checkout-icons.php
+++ includes/block-checkout-icons.php
@@ -6,27 +6,30 @@ $paymentMethodsPath = TZP_PUBLIC_ASSETS_DIR . '/images/payment_methods.png';
 <script>
   document.addEventListener("DOMContentLoaded", function () {
     const checkElementAndAddIcons = () => {
-      const labelElement = document.querySelector("#radio-control-wc-payment-method-options-tazapay__label");
-
-      if (labelElement) {
-        const htmlContent = `
-                <div class="tw-mt-2 tw-flex tw-flex-wrap tw-gap-2">
-                    <div>
-                        <img src="<?php echo $logoPath; ?>" alt="tazapay" />
+        const labelElement = document.querySelector("#radio-control-wc-payment-method-options-tazapay__label");
+        
+        if (labelElement) {
+            // Check if icons are already added
+            if (!labelElement.nextElementSibling?.classList.contains("tw-mt-2")) {
+                const htmlContent = `
+                    <div class="tw-mt-2 tw-flex tw-flex-wrap tw-gap-2">
+                        <div>
+                            <img src="<?php echo $logoPath; ?>" alt="tazapay" />
+                        </div>
+                        <div>
+                            <img src="<?php echo $paymentMethodsPath; ?>" alt="local payment" />
+                        </div>
                     </div>
-                    <div>
-                        <img src="<?php echo $paymentMethodsPath; ?>" alt="local payment" />
-                    </div>
-                </div>
-            `;
-        labelElement.insertAdjacentHTML('afterEnd', htmlContent);
-      } else {
-        // If the element is not found, try again after a short delay
-        setTimeout(checkElementAndAddIcons, 100); // Check again after 100ms
-      }
+                `;
+                labelElement.insertAdjacentHTML('afterEnd', htmlContent);
+            }
+        } else {
+            // If the element is not found, try again after a short delay
+            setTimeout(checkElementAndAddIcons, 100); // Check again after 100ms
+        }
     };
 
     checkElementAndAddIcons(); // Initial check
-  });
+});
 
 </script>
