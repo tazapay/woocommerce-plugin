841290115b23c59ad834258d05f4a43f7d7ada67
Suraj <suraj@tazapay.com>
2024-10-29 12:48:16 +0530

    fix icons

--- includes/block-checkout-icons.php
+++ includes/block-checkout-icons.php
@@ -5,21 +5,28 @@ $paymentMethodsPath = TZP_PUBLIC_ASSETS_DIR . '/images/payment_methods.png';
 
 <script>
   document.addEventListener("DOMContentLoaded", function () {
-    const htmlContent = `
-        <div class="tw-mt-2 tw-flex tw-flex-wrap tw-gap-2">
-            <div>
-                <img src="<?php echo $logoPath; ?>" alt="tazapay" />
-            </div>
-            <div>
-                <img src="<?php echo $paymentMethodsPath; ?>" alt="local payment" />
-            </div>
-        </div>
-    `;
+    const checkElementAndAddIcons = () => {
+      const labelElement = document.querySelector("#radio-control-wc-payment-method-options-tazapay__label");
 
-    const labelElement = document.querySelector("#radio-control-wc-payment-method-options-tazapay__label");
-    if (labelElement) {
-      labelElement.insertAdjacentHTML('afterEnd', htmlContent);
-    }
+      if (labelElement) {
+        const htmlContent = `
+                <div class="tw-mt-2 tw-flex tw-flex-wrap tw-gap-2">
+                    <div>
+                        <img src="<?php echo $logoPath; ?>" alt="tazapay" />
+                    </div>
+                    <div>
+                        <img src="<?php echo $paymentMethodsPath; ?>" alt="local payment" />
+                    </div>
+                </div>
+            `;
+        labelElement.insertAdjacentHTML('afterEnd', htmlContent);
+      } else {
+        // If the element is not found, try again after a short delay
+        setTimeout(checkElementAndAddIcons, 100); // Check again after 100ms
+      }
+    };
+
+    checkElementAndAddIcons(); // Initial check
   });
 
 </script>
