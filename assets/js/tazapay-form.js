95dbf5c89e29af909bd48d3c2b5a04a599f158e6
Suraj <suraj@tazapay.com>
2024-11-11 10:50:22 +0530

    fix fields

--- assets/js/tazapay-form.js
+++ assets/js/tazapay-form.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function ($) {
 	function toggelMode() {
-		if ($("#woocommerce_tz_tazapay_select_env_mode").val() === "Production") {
+		if ($("#woocommerce_tazapay_select_env_mode").val() === "Production") {
 			$(".tazapay_live_mode_fields").closest("tr").show();
 			$(".tazapay_test_mode_fields").closest("tr").hide();
 		} else {
@@ -9,7 +9,7 @@ jQuery(document).ready(function ($) {
 		}
 	}
 	toggelMode();
-	$("#woocommerce_tz_tazapay_select_env_mode").change(function () {
+	$("#woocommerce_tazapay_select_env_mode").change(function () {
 		toggelMode();
 	});
 });
